e7c2dc1dd2488fc64cdbb1bbfa4dfa4e93b69917
Tejaswini Hirudkar <121488475+TejaswiniHirudkar@users.noreply.github.com>
2023-06-16 20:08:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var infodiv=document.getElementById("brower-info");
+var name=navigator.appName;
+var version=navigator.appVersion;
+var message="You are using "+name+" version "+version;
+infodiv.textContent=message;
